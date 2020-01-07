c69a6c4e779a53d85b42d7d557afc33907910c3b
Rohit Satyam <rohitsatyam102@gmail.com>
2020-01-07 19:04:03 +0530

    Create SRR_fetch.sh

---
+++ SRR_fetch.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+## The following script uses conda environment and install entrez utilities if absent. If you don't work with conda comment out the conda command ##
+
+##................................................................###
+## To check if entrez-direct is installed ##
+
+if ! type "efetch" > /dev/null; then            ## check if 
+  echo "E-utilities Not Found."
+  echo "installing entrez-direct from conda"
+  conda install -c bioconda entrez-direct
+fi
+##................................................................##
+
+##To input File
+
+input=$1
+
+##................................................................##
+
+## To convert GSM number to SRR number ##
+
+for GSM in `cat $input`
+do
+echo "Retriving $GSM from NCBI GEO";
+esearch -db sra -query $GSM | efetch -format docsum | xtract -pattern DocumentSummary -element Run@acc >> pappu;
+fastq-dump --outdir fastq --gzip --skip-technical  --origfmt --dumpcs --readids --read-filter pass --dumpbase --split-3 --clip SRR_ID
+echo 'retrival_successful_FASTQ DUmped';
+done
